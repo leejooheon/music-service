1781ca15319a3ed144adc352fec53ea0857762ef
Jooheon <jooheon.lee@bugs.co.kr>
2024-03-09 23:18:57 +0900

    ♻️Refactor: migrate to submodule

---
+++ src/main/java/com/jooheon/features/musicservice/MusicService.kt
@@ -0,0 +1,287 @@
+package com.jooheon.features.musicservice
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.IBinder
+import androidx.annotation.OptIn
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DataSourceBitmapLoader
+import androidx.media3.datasource.ResolvingDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.extractor.DefaultExtractorsFactory
+import androidx.media3.session.CacheBitmapLoader
+import androidx.media3.session.MediaLibraryService
+import androidx.media3.session.MediaSession
+import com.jooheon.features.musicservice.data.MediaItemProvider
+import com.jooheon.features.musicservice.notification.CustomMediaNotificationCommand
+import com.jooheon.features.musicservice.notification.CustomMediaNotificationProvider
+import com.jooheon.features.musicservice.playback.PlaybackCacheManager
+import com.jooheon.features.musicservice.playback.PlaybackUriResolver
+import com.jooheon.features.musicservice.playback.PlaybackListener
+import com.jooheon.features.musicservice.player.ToyPlayer
+import com.jooheon.features.musicservice.usecase.PlaybackErrorUseCase
+import com.jooheon.features.musicservice.usecase.PlaybackLogUseCase
+import com.jooheon.features.musicservice.usecase.PlaybackUseCase
+import com.jooheon.toyplayer.features.musicservice.BuildConfig
+import com.jooheon.toyplayer.features.musicservice.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flow
+import timber.log.Timber
+import javax.inject.Inject
+import kotlin.system.exitProcess
+
+@OptIn(UnstableApi::class)
+@AndroidEntryPoint
+class MusicService: MediaLibraryService() {
+    private val TAG = MusicService::class.java.simpleName + "@" + "Main"
+
+    @Inject
+    lateinit var playbackListener: PlaybackListener
+
+    @Inject
+    lateinit var singleTopActivityIntent: Intent
+
+    @Inject
+    lateinit var playbackUriResolver: PlaybackUriResolver
+
+    @Inject
+    lateinit var playbackCacheManager: PlaybackCacheManager
+
+    @Inject
+    lateinit var mediaItemProvider: MediaItemProvider
+
+    @Inject
+    lateinit var musicStateHolder: MusicStateHolder
+
+    @Inject
+    lateinit var playbackLogUseCase : PlaybackLogUseCase
+
+    @Inject
+    lateinit var playbackUseCase : PlaybackUseCase
+
+    @Inject
+    lateinit var playbackErrorUseCase : PlaybackErrorUseCase
+
+    private lateinit var mediaSession: MediaLibrarySession
+    private lateinit var mediaLibrarySessionCallback: com.jooheon.features.musicservice.MediaLibrarySessionCallback
+    private lateinit var customMediaNotificationProvider: CustomMediaNotificationProvider
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private var notificationManager: NotificationManager? = null
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo) = mediaSession
+
+    override fun onCreate() {
+        Timber.tag(LifecycleTAG).d( "onCreate")
+        super.onCreate()
+
+        initMediaSession()
+        initNotification()
+        initListener()
+        initUseCase()
+
+        collectStates()
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        Timber.tag(LifecycleTAG).d( "onTaskRemoved - 1")
+        if (!mediaSession.player.playWhenReady) {
+            Timber.tag(LifecycleTAG).d( "onTaskRemoved - 2")
+
+            // If the player isn't set to play when ready, the service is stopped and resources released.
+            // This is done because if the app is swiped away from recent apps without this check,
+            // the notification would remain in an unresponsive state.
+            // Further explanation can be found at: https://github.com/androidx/media/issues/167#issuecomment-1615184728
+            stopSelf()
+        }
+    }
+
+    override fun onDestroy() {
+        Timber.tag(LifecycleTAG).d( "onDestroy")
+        release()
+        super.onDestroy()
+    }
+
+    private fun release() {
+        playbackUriResolver.release()
+        playbackCacheManager.release()
+        mediaLibrarySessionCallback.release()
+
+        mediaSession.release()
+        mediaSession.player.release()
+
+        clearListener()
+        serviceScope.cancel()
+
+//        handleMedia3Bug()
+    }
+
+    private fun handleMedia3Bug() { // TODO: CleanUp
+        /**
+         * process가 종료되지 않는 버그.. 짱난다 demo-session도 동일
+         * we should have something else instead\
+         * https://github.com/androidx/media/issues/805
+         **/
+        notificationManager?.cancelAll()
+        exitProcess(0)
+    }
+
+    private fun initPlayer(): Player {
+        playbackCacheManager.init()
+        playbackUriResolver.init(playbackCacheManager)
+
+        val mediaSourceFactory = DefaultMediaSourceFactory(
+            ResolvingDataSource.Factory(
+                playbackCacheManager.cacheDataSource(),
+                playbackUriResolver
+            ),
+            DefaultExtractorsFactory()
+        )
+
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .setUsage(C.USAGE_MEDIA)
+            .build()
+
+        val exoPlayer = ExoPlayer.Builder(applicationContext)
+            .setMediaSourceFactory(mediaSourceFactory)
+            .setAudioAttributes(audioAttributes, true) // AudioFocus가 변경될때
+            .setHandleAudioBecomingNoisy(true) // 재생 주체가 변경될때 정지 (해드폰 -> 스피커)
+            .build()
+
+        return ToyPlayer(exoPlayer)
+    }
+
+    private fun initNotification() {
+        customMediaNotificationProvider = CustomMediaNotificationProvider(
+            context = this,
+            notificationIdProvider = { NOTIFICATION_ID },
+            channelId = NOTIFICATION_CHANNEL_ID,
+            channelNameResourceId = R.string.playing_notification_name,
+        )
+        setMediaNotificationProvider(customMediaNotificationProvider)
+    }
+
+    private fun initMediaSession() {
+        val player = initPlayer()
+
+        mediaLibrarySessionCallback = com.jooheon.features.musicservice.MediaLibrarySessionCallback(
+            context = this,
+            mediaItemProvider = mediaItemProvider,
+        )
+
+        mediaSession = MediaLibrarySession.Builder(
+            /** service  **/this,
+            /** player   **/ player,
+            /** callback **/ mediaLibrarySessionCallback,
+        )
+        .setSessionActivity(getSingleTopActivity())
+//        .setBitmapLoader(CoilBitmapLoader(this, serviceScope))
+        .setBitmapLoader(CacheBitmapLoader(DataSourceBitmapLoader(/* context= */ this)))
+        .build()
+    }
+
+    private fun initListener() {
+        mediaSession.player.addListener(playbackListener)
+    }
+
+    private fun initUseCase() = serviceScope.launch {
+        playbackUseCase.initialize(mediaSession.player, serviceScope)
+        playbackLogUseCase.initialize(serviceScope)
+        playbackErrorUseCase.initialize(serviceScope)
+    }
+
+    private fun collectStates() {
+        serviceScope.launch {
+            launch {
+                combine(
+                    musicStateHolder.repeatMode,
+                    musicStateHolder.shuffleMode
+                ) { repeatMode, shuffleMode ->
+                    repeatMode to shuffleMode
+                }.collectLatest { (repeatMode, shuffleMode) ->
+                    mediaSession.setCustomLayout(
+                        CustomMediaNotificationCommand.layout(
+                            context = this@MusicService,
+                            shuffleMode = shuffleMode,
+                            repeatMode = repeatMode
+                        )
+                    )
+                }
+            }
+
+            launch {
+                musicStateHolder.isPlaying.collectLatest { isPlaying ->
+                    if(isPlaying) {
+                        withContext(Dispatchers.Main) {
+                            pollCurrentDuration(mediaSession.player).collect {
+                                    value -> musicStateHolder.onCurrentDurationChanged(value)
+                            }
+                        }
+                    }
+                }
+            }
+
+            launch {
+                playbackErrorUseCase.autoPlayChannel.collectLatest {
+                    val player = mediaSession.player
+                    if(!player.isPlaying) player.play()
+                }
+            }
+        }
+    }
+
+    private fun pollCurrentDuration(player: Player) = flow {
+        while (player.isPlaying && (player.currentPosition + POLL_INTERVAL_MSEC <= player.duration)) {
+            emit(player.currentPosition)
+            delay(POLL_INTERVAL_MSEC)
+        }
+    }.conflate()
+
+    private fun getSingleTopActivity(): PendingIntent {
+        return PendingIntent.getActivity(
+            this,
+            0,
+            singleTopActivityIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        Timber.tag(LifecycleTAG).d("onBind: ${intent?.component}")
+        return super.onBind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Timber.tag(LifecycleTAG).d("onUnbind: ${intent?.component}")
+        return super.onUnbind(intent)
+    }
+    override fun onRebind(intent: Intent?) {
+        Timber.tag(LifecycleTAG).d("onRebind: ${intent?.component}")
+        super.onRebind(intent)
+    }
+    companion object {
+        private const val PACKAGE_NAME = BuildConfig.LIBRARY_PACKAGE_NAME
+        private const val POLL_INTERVAL_MSEC = 500L
+
+        private const val LifecycleTAG = "ServiceLifecycle"
+
+        const val NOTIFICATION_ID = 234
+        const val NOTIFICATION_CHANNEL_ID = "Jooheon_player_notification"
+
+        const val CYCLE_REPEAT = "$PACKAGE_NAME.cycle_repeat"
+        const val TOGGLE_SHUFFLE = "$PACKAGE_NAME.toggle_shuffle"
+    }
+}
---
+++ src/main/java/com/jooheon/features/musicservice/ext/PlayerMessageExtension.kt
@@ -0,0 +1,59 @@
+package com.jooheon.features.musicservice.ext
+
+import android.media.session.PlaybackState
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+
+fun Int.playbackState() = when(this) {
+    PlaybackState.STATE_STOPPED -> "STATE_STOPPED"
+    PlaybackState.STATE_PAUSED -> "STATE_PAUSED"
+    PlaybackState.STATE_PLAYING -> "STATE_PLAYING"
+    PlaybackState.STATE_FAST_FORWARDING -> "STATE_FAST_FORWARDING"
+    PlaybackState.STATE_REWINDING -> "STATE_REWINDING"
+    PlaybackState.STATE_BUFFERING -> "STATE_BUFFERING"
+    PlaybackState.STATE_ERROR -> "STATE_ERROR"
+    PlaybackState.STATE_CONNECTING -> "STATE_CONNECTING"
+    PlaybackState.STATE_SKIPPING_TO_PREVIOUS -> "STATE_SKIPPING_TO_PREVIOUS"
+    PlaybackState.STATE_SKIPPING_TO_NEXT -> "STATE_SKIPPING_TO_NEXT"
+    PlaybackState.STATE_SKIPPING_TO_QUEUE_ITEM -> "STATE_SKIPPING_TO_QUEUE_ITEM"
+    else -> "STATE_NONE"
+}
+fun Int.playerState() = when(this) {
+    Player.STATE_IDLE -> "STATE_IDLE"
+    Player.STATE_BUFFERING -> "STATE_BUFFERING"
+    Player.STATE_READY -> "STATE_READY"
+    Player.STATE_ENDED -> "STATE_ENDED"
+    else -> "STATE_NONE"
+}
+
+fun @Player.State Int.toPlaybackState(playWhenReady: Boolean): Int {
+    return when(this) {
+        Player.STATE_BUFFERING -> if (playWhenReady) PlaybackState.STATE_BUFFERING else PlaybackState.STATE_PAUSED
+        Player.STATE_READY -> if (playWhenReady) PlaybackState.STATE_PLAYING else PlaybackState.STATE_PAUSED
+        Player.STATE_ENDED -> PlaybackState.STATE_STOPPED
+        Player.STATE_IDLE -> PlaybackState.STATE_NONE
+        else -> PlaybackState.STATE_NONE
+    }
+}
+
+fun Int.mediaItemTransitionReason() = when(this) {
+    Player.MEDIA_ITEM_TRANSITION_REASON_REPEAT -> "MEDIA_ITEM_TRANSITION_REASON_REPEAT"
+    Player.MEDIA_ITEM_TRANSITION_REASON_AUTO, -> "MEDIA_ITEM_TRANSITION_REASON_AUTO"
+    Player.MEDIA_ITEM_TRANSITION_REASON_SEEK -> "MEDIA_ITEM_TRANSITION_REASON_SEEK"
+    Player.MEDIA_ITEM_TRANSITION_REASON_PLAYLIST_CHANGED -> "MEDIA_ITEM_TRANSITION_REASON_PLAYLIST_CHANGED"
+    else -> throw IllegalStateException("Unknown MediaItemTransitionReason")
+}
+fun Int.playWhenReadyChangeReason() = when(this) {
+    Player.PLAY_WHEN_READY_CHANGE_REASON_USER_REQUEST -> "PLAY_WHEN_READY_CHANGE_REASON_USER_REQUEST"
+    Player.PLAY_WHEN_READY_CHANGE_REASON_AUDIO_FOCUS_LOSS, -> "PLAY_WHEN_READY_CHANGE_REASON_AUDIO_FOCUS_LOSS"
+    Player.PLAY_WHEN_READY_CHANGE_REASON_AUDIO_BECOMING_NOISY -> "PLAY_WHEN_READY_CHANGE_REASON_AUDIO_BECOMING_NOISY"
+    Player.PLAY_WHEN_READY_CHANGE_REASON_REMOTE -> "PLAY_WHEN_READY_CHANGE_REASON_REMOTE"
+    Player.PLAY_WHEN_READY_CHANGE_REASON_END_OF_MEDIA_ITEM -> "PLAY_WHEN_READY_CHANGE_REASON_END_OF_MEDIA_ITEM"
+    Player.PLAY_WHEN_READY_CHANGE_REASON_SUPPRESSED_TOO_LONG -> "PLAY_WHEN_READY_CHANGE_REASON_SUPPRESSED_TOO_LONG"
+    else -> throw IllegalStateException("Unknown MediaItemTransitionReason")
+}
+fun Int.timelineChangeReason() = when(this) {
+    Player.TIMELINE_CHANGE_REASON_PLAYLIST_CHANGED -> "TIMELINE_CHANGE_REASON_PLAYLIST_CHANGED"
+    Player.TIMELINE_CHANGE_REASON_SOURCE_UPDATE, -> "PLAY_WHEN_READY_CHANGE_REASON_AUDIO_FOCUS_LOSS"
+    else -> throw IllegalStateException("Unknown MediaItemTransitionReason")
+}
